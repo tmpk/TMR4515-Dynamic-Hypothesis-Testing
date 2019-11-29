a801d8f7b50a749d3c304f7b6acf704834c62cf0
tmpk <tmp.klausen@gmail.com>
2019-11-29 22:23:47 +0100

    final version?

--- get_ss_KF.m
+++ get_ss_KF.m
@@ -1,11 +1,17 @@
-function [Ad, Bd, Kgain, P] = get_ss_KF(A, B, G, C, Q, R, k2, k5, Ts)
+function [kest, Ad, Bd, K, P] = get_ss_KF(A, B, G, C, Q, R, k2, k5, Ts)
+% This function returns a state space representation of a Kalman filter, as
+% well as the steady-state gain matrix and covariance matrix of the filter.
+% It also returns the discrete-time system matrices Ad and Bd. 
+% the input is the continuous-time system matrices A, B, G and C, covariance
+% matrix Q and R of process and measurement noise, and the assumed values of
+% the unknown parameters k2 and k5. Ts is the timestep
 
 % discrete system matrices
-[Ad, Bd] = c2d(A,B,Ts);     % discrete A and B matrices
+[Ad, Bd] = c2d(A,B,Ts);     
 
 % continuous state space model
 sys = ss(A,[B G], C, 0);
 
 % discrete Kalman estimator for the continuous plant
-% K_inf is the Kalman gain
-[kest, Kgain, P, M, Z] = kalmd(sys, Q, R, Ts);
+% K is the Kalman gain, P covariance error matrix
+[kest, K, P, ~, ~] = kalmd(sys, Q, R, Ts);
